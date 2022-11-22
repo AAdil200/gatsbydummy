447d2bf1a622e26ec31b7130eee91a6a2232f688
Shaikh Aadil010 <shaikh.pro010@gmail>
2022-11-22 18:03:02 +0530

    AboutPage

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function about(props) {
+  return (
+    <div>
+      <h1>Insha allah neww Journey will success soon</h1>
+    </div>
+  );
+}
+
+export default about;
